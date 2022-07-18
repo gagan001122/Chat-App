4b23d19e6ec453cc4e1f27a9a3f0b543f564147e
gagan001122 <gaganrajput8437@gmail.com>
2022-07-18 13:13:28 +0530

    getting started

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'rsuite/styles/index.less';
+import { Button } from 'rsuite';
+import './styles/main.scss';
+
+function App() {
+  return <div>hello</div>;
+}
+
+export default App;
