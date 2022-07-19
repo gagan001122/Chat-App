8d633957f7c36a79a9435e55cfe4535e0ebdae46
gagan001122 <gaganrajput8437@gmail.com>
2022-07-19 15:06:27 +0530

    created signin page and signin with google and facebook

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,14 +1,17 @@
 import firebase from 'firebase/app';
-
+import 'firebase/auth';
+import 'firebase/database';
 const config = {
     apiKey: "AIzaSyB3t4IrpRrKQHVaFUW3qrLP44q0VQQTYuA",
     authDomain: "chat-web-app-7b030.firebaseapp.com",
     projectId: "chat-web-app-7b030",
     storageBucket: "chat-web-app-7b030.appspot.com",
+    databaseURL: "https://chat-web-app-7b030-default-rtdb.asia-southeast1.firebasedatabase.app/",
     messagingSenderId: "694156485994",
     appId: "1:694156485994:web:568516baec8e7d1d0f3113",
     measurementId: "G-N1HTFBPG45"
   };
 
 const app = firebase.initializeApp(config);
-
+export const auth = app.auth();
+export const database = app.database();
