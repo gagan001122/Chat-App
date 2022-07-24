b5459e683bf28665b6916a4752f928da3d7393ca
gagan001122 <gaganrajput8437@gmail.com>
2022-07-24 14:32:28 +0530

    finished real time presence functionality

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,6 @@
 import { useState, useCallback, useEffect } from 'react';
+import { database } from './firebase';
+
 export function useModalState(defaultValue = false) {
   const [isOpen, setIsOpen] = useState(defaultValue);
 
@@ -9,11 +11,11 @@ export function useModalState(defaultValue = false) {
 }
 
 export const useMediaQuery = query => {
-    const [matches, setMatches] = useState(
-      () => window.matchMedia(query).matches
-    );
-  
-useEffect(() => {
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches
+  );
+
+  useEffect(() => {
     const queryList = window.matchMedia(query);
     setMatches(queryList.matches);
 
@@ -25,3 +27,25 @@ useEffect(() => {
 
   return matches;
 };
+
+export function usePresence(uid) {
+  const [presence, setPresence] = useState(null);
+
+  useEffect(() => {
+    const userStatusRef = database.ref(`/status/${uid}`);
+
+    userStatusRef.on('value', snap => {
+      if (snap.exists()) {
+        const data = snap.val();
+
+        setPresence(data);
+      }
+    });
+
+    return () => {
+      userStatusRef.off();
+    };
+  }, [uid]);
+
+  return presence;
+}
